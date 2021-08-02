587d194c62825ea8275fb18cad40a90fea538b1b
jdwalsh138 <84231343+jdwalsh138@users.noreply.github.com>
2021-08-03 00:29:49 +1000

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,138 +1,220 @@
-const home = document.getElementsByClassName("home");
-const overview = document.getElementsByClassName("overview");
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCountEl = document.getElementById("question-count");
-const scoreEl = document.getElementById("score");
-const startBtn = document.getElementById("start-quiz");
-const scoresBtn = document.getElementById("show-scores");
-const scoreTitle = document.getElementById("score-title");
-const questionTitle = document.getElementById("question-title");
-const intro = document.getElementById("intro");
+var timerEl = document.querySelector("#timer")
+var buttonEl = document.querySelector("#startButton")
+var startEl = document.querySelector("#start")
+var quizEl = document.querySelector("#quiz")
+var questionEl = document.querySelector("#question")
+var answerEl = document.querySelector("#answers")
+var doneEl = document.querySelector('#done')
+var submitButton = document.querySelector("#submit")
+var scoreEl = document.querySelector("#score")
+var backButton  = document.querySelector("#back")
+var wrongEl = document.querySelector("#wrong")
+var correctEl = document.querySelector("#correct")
+var userScoreEl = document.querySelector("#userScore")
+var nameInput=document.querySelector("#name")
+var userScores = document.querySelector("#userScores")
+var clearButton = document.querySelector("#clear")
+var highScoresWl = document.querySelector("#highScores")
 
+var questionNumber = 0
 
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+var timer = 75
+var finalScore = 0
 
-startBtn.addEventListener("click", e =>{
-    startGame();
-})
+var questions = [{
+    question: "Commonly used data types DO NOT include:",
+    answer: ["Strings","booleans", "alerts", "numbers" ],
+    correct:2,
+},
+{
+    question: "The condition in an if/else statement is enclose within ____.",
+    answer: ["quotes","curly brackets", "parentheses", "square brackets" ],
+    correct:2,
+}
+,
+{
+    question: "Arrays in JavaScript can be used to store _____.",
+    answer: ["numbers and strings","other arrays", "booleans", "all of the above" ],
+    correct:4,
+},
+{
+    question: "String values must be enclosed within _______ when being assigned to variables.",
+    answer: ["commas","curly brackets", "parentheses", "square brackets" ],
+    correct:3,
+}
+]
 
-scoresBtn.addEventListener("click", e =>{
-    showHighscores();
-})
+function toggleDone() {
+    if(doneEl.style.display == "none"){
+        doneEl.style.display = "block"
+        userScoreEl.textContent = "Your final score " + finalScore
+    }
+    else{
+        doneEl.style.display = "none"
+    }
+}
 
-const questions = [
-    {
-        q: 'Inside which HTML element do we put the JavaScript?',
-            c1: '<js>',
-            c2: '<script>',
-            c3: '<scripting>',
-            c4: '<javascript>',
-            a: 2
-    },
-    {
-        q: 'What is the correct JavaScript syntax to change the content of the HTML element below?',
-            c1: 'document.getElementById("demo").innerHTML = "Hello World!";',
-            c2: '#demo.innerHTML = "Hello World!";',
-            c3: 'document.getElementByName("p").innerHTML = "Hello World!";',
-            c4: 'document.getElement("p").innerHTML = "Hello World!";',
-            a: 1
-    },
-    {
-        q: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-            c1: 'The <head> section',
-            c2: 'The <body> section',
-            c3: 'Both the <head> section and the <body> section are correct',
-            c4: 'None are correct',
-            a: 3
-    },
-    {
-        q: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-            c1: '<script name="xxx.js">',
-            c2: '<script src="xxx.js">',
-            c3: '<script href="xxx.js">',
-            c4: '<script title="xxx.js">',
-            a: 2
-    },
-    {
-        q: 'The external Javascript file must contain the <script> tag.',
-            c1: 'True',
-            c2: 'False',
-            a: 2
-    },
+function toggleScore() {
+    while (userScores.lastElementChild) {
+        userScores.removeChild(userScores.lastElementChild);
+      }
+    if(scoreEl.style.display == "none"){
+        scoreEl.style.display = "block"
 
-];
+        Object.keys(localStorage).forEach(element => {
+            var user = document.createElement("li")
+            user.textContent = element + " - " +localStorage.getItem(element)
+            user.setAttribute('class', "bg-secondary text-white p-1 mb-2")
+            userScores.appendChild(user)
+        });
 
-const correctBonus = 20;
-const maxQuestions = 5;
+    }
+    else{
+        scoreEl.style.display = "none"
+    }
+}
 
+function toggleStart() {
+    if(startEl.style.display == "none"){
+        startEl.style.display = "block"
+    }
+    else{
+        startEl.style.display = "none"
+    }
+}
 
+function toggleQuiz() {
+    if(quizEl.style.display == "none"){
+        quizEl.style.display = "block"
+    }
+    else{
+        quizEl.style.display = "none"
+    }
+}
 
-startGame = () => {
-  questionCounter = 0;
-  score = 0;
-  scoreEl.innerText = score
-  availableQuestions = [...questions];
-  getNewQuestion();
-};
+function toggleWrong() {
+    if(wrongEl.style.display == "none"){
+        wrongEl.style.display = "block"
+        var correctTime = 1
+        var timerInterval = setInterval(function() {
+            correctTime--
+            if(correctTime == 0){
+                clearInterval(timerInterval)
+                toggleWrong()
+            }
+        }, 1000)
+    }
+    else{
+        wrongEl.style.display = "none"
+    }
+}
 
-getNewQuestion = () => {
-  if (
-    availableQuestions.length === 0 ||
-    questionCounter >= maxQuestions.length
-  ) {
-    showHighscores();
-  }
-  questionCounter++;
-  questionCountEl.innerText = `${questionCounter}/${maxQuestions}`;
+function toggleCorrect() {
+    if(correctEl.style.display == "none"){
+        correctEl.style.display = "block"
+        var correctTime = 1
+        var timerInterval = setInterval(function() {
+            correctTime--
+            if(correctTime == 0){
+                clearInterval(timerInterval)
+                toggleCorrect()
+            }
+        
+        }, 1000)
+    }
+    else{
+        correctEl.style.display = "none"
+    }
+}
 
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
-  question.innerText = currentQuestion.q;
-
-  choices.forEach(choice => {
-    const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["c" + number];
-  });
-  availableQuestions.splice(questionIndex, 1);
-  acceptingAnswers = true;
-};
-
-choices.forEach(choice => {
-  choice.addEventListener("click", e => {
-    console.log(e.target);
-    if (!acceptingAnswers) return;
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
-
-    const answerStatus =
-      selectedAnswer == currentQuestion.a ? "correct" : "incorrect";
-    selectedChoice.parentElement.classList.add(answerStatus);
-
-    if (answerStatus === "correct") {
-      updateScore(correctBonus);
+function loadQuestion() {
+    while (answerEl.lastElementChild) {
+        answerEl.removeChild(answerEl.lastElementChild);
+      }
+    if(questions[questionNumber]){
+        questionEl.textContent = questions[questionNumber].question
+        questions[questionNumber].answer.forEach(function(element, i) {
+            var answers = document.createElement("button")
+            answers.textContent =element
+            console.log(i)
+            answers.setAttribute("class","btn btn-primary  p-3 m-2 ")
+            answers.setAttribute("data-index", i)
+            answerEl.appendChild(answers)
+        });
+    }
+    else{
+        finalScore = timer
+        timer = 1
     }
 
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(answerStatus);
-      getNewQuestion();
-    }, 1000);
-  });
-});
-
-startGame();
-
-updateScore = num => {
-  score += num;
-  scoreEl.innerText = score;
-};
-
-
-showHighscores = ()=>{
-
 }
+
+submitButton.addEventListener("click", function () {
+    toggleDone()
+    var name = nameInput.value.trim()
+    localStorage.setItem(name, timer)
+    toggleScore()
+})
+
+backButton.addEventListener("click", function(){
+    toggleScore()
+    toggleStart()
+    timerEl.textContent = "Time: 75"
+})
+
+function time(){
+    console.log(timer)
+    var timerInterval = setInterval(function() {
+        timer--
+        timerEl.textContent = "Time: " + timer
+        if(timer == 0){
+            clearInterval(timerInterval)
+            toggleQuiz()
+            toggleDone()
+        }
+    }, 1000)
+}
+
+buttonEl.addEventListener("click", function(){
+    timer = 75
+    questionNumber = 0
+    console.log(timer, questionNumber)
+    time()
+    toggleStart() 
+    toggleQuiz()
+    loadQuestion()
+    
+  answerEl.addEventListener("click", function (event) {
+        var element = event.target
+        if(element.matches("button")){
+            var index = element.getAttribute("data-index")
+            console.log(index, questions[questionNumber].correct)
+            if(index == questions[questionNumber].correct){
+                questionNumber++
+                toggleCorrect()
+                loadQuestion()
+            }
+            else{
+                questionNumber++
+                timer -= 15
+                loadQuestion()
+                toggleWrong()
+            }
+        }
+    })
+})
+
+clearButton.addEventListener("click", function(){
+    localStorage.clear()
+    while(userScores.lastElementChild){
+        userScores.removeChild(userScores.lastElementChild)
+    }
+    toggleScore()
+    toggleScore()
+})
+
+highScoresWl.addEventListener("click", function(){
+    toggleStart()
+    toggleScore()
+
+})
